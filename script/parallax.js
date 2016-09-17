b8d1e517f478454716413ba5bb7f48ccaa4d4825
Antoha5001 <plantsvzzombi@mail.ru>
2016-09-17 17:06:09 +0700

    edit script

--- script/parallax.js
+++ script/parallax.js
@@ -1,30 +1,36 @@
-$(document).ready(function(){
-	
-	setTimeout(function(){
-		$('.header__logo').addClass('is-visibile');
-		
-		var wScroll = $(window).scrollTop();
-		
-		
-		if(wScroll > $('.production-box').offset().top - $(window).height()){
-			$('.production__item').each(function(e){
-				setTimeout(function(){					
-					$('.production__item').eq(e).addClass('is-visibile');
-				},350*(e+1))
-			});
-		}
-	}, 500);
-		
-		
-	$(window).scroll(function(){	
-		
-	var wScroll = $(window).scrollTop();
-		if(wScroll > ($('.footer').offset().top - $(window).height()+50)){
-			$('.footer__logo').addClass('is-visibile');	
-		}	else{
-			$('.footer__logo').removeClass('is-visibile');
-		}
-		
-	console.log(wScroll+":"+($('.footer').offset().top- $(window).height()+250));
-	});
+$(document).ready(function() {
+
+    setTimeout(function() {
+        $('.header__logo').addClass('is-visibile');
+
+        var wScroll = $(window).scrollTop();
+        if(!$('.production__item').hasClass('is-visibile') && wScroll > $('.production-box').offset().top - $(window).height()){
+            $('.production__item').each(function(e) {
+                setTimeout(function() {
+                    $('.production__item').eq(e).addClass('is-visibile');
+                }, 350 * (e + 1))
+            });
+        }
+
+
+    }, 500);
+
+
+    $(window).scroll(function() {
+
+        var wScroll = $(window).scrollTop();
+        if (wScroll > ($('.footer').offset().top - $(window).height() + 50)) {
+            $('.footer__logo').addClass('is-visibile');
+        } else {
+            $('.footer__logo').removeClass('is-visibile');
+        }
+        if (wScroll > $('.production-box').offset().top - $(window).height()) {
+            $('.production__item').each(function(e) {
+                setTimeout(function() {
+                    $('.production__item').eq(e).addClass('is-visibile');
+                }, 350 * (e + 1))
+            });
+        }
+
+    });
 });
