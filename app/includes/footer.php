d8734976a5dfafa69d51a445e925d26e5c768aca
Antoha5001 <plantsvzzombi@mail.ru>
2017-05-25 19:05:08 +0700

    upload files

---
+++ app/includes/footer.php
@@ -0,0 +1,23 @@
+	<section class="footer">
+			<div class="content content_flex">
+				<div class="footer__cols"><span class="footer__zagolovok">Тел.: +7 (3852) 62-33-44<br/>
+E-mail:<a href="mailto:623344@mail.ru" class="link link_white"> 623344@mail.ru</a><br/>
+Адрес: Алтайский край, г. Барнаул, <br/>улица Южные Мастерские, д. 14 Б </span>
+
+				</div>
+				<div class="footer__cols">
+					<a href="/" class="footer__logo">
+						<img src="images/logo_white.svg" alt="">
+					</a>
+				</div>
+				<div class="footer__cols"><span class="footer__zagolovok footer__zagolovok_right">2017 &copy ООО «Типография Графика»</span>
+				</div>
+
+			</div>
+
+		</section>
+	</div>
+<script src="script/scripts.min.js"></script>
+</body>
+
+</html>
---
+++ app/includes/head.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+
+<head>
+  <title>Заголовок</title>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css" />
+  <link rel="stylesheet" href="css/mystyle.min.css" media="screen">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+  <link rel="icon" href="images/favicon.png" type="image/x-icon">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+  <!--[if lt IE 9]>
+<link rel="stylesheet" href="/reject/reject.css" media="all" />
+<script type="text/javascript" src="reject/reject.min.js"></script>
+<![endif]-->
+
+</head>
+<body>
+  <div class="conteiner">
+    <div class="main">
+      <div class="all">
+        <section class="header-box">
+          <div class="content content_flex">
+            <div class="top-menu-small">
+              <ul class="top-menu-small__items">
+                <li class="top-menu-small__item"><a href="" class="top-menu-small__link">Ffff</a></li>
+                <li class="top-menu-small__item"><a href="" class="top-menu-small__link">Vvvv</a></li>
+                <li class="top-menu-small__item"><a href="" class="top-menu-small__link">Bbbb</a></li>
+              </ul>
+            </div>
+            <div class="header__top-cols header__top-cols_logo wow fadeIn" data-wow-delay="0.2s" data-wow-duration="1.3s">
+              <a href="/" class="header__logo"> <img src="images/logo.svg" alt=""> </a>
+            </div>
+            <div class="header__top-cols header__top-cols_phone">
+              <p class="header__phone">+7 (3852) 623399</p>
+              <p class="header__button">
+                заказать звонок
+              </p>
+            </div>
+            <!--
+						<div class="header__top-cols header__top-cols_email">
+						</div>
+-->
+            <div class="header__top-cols header__top-cols_addres"> <a href="mailto:623344@mail.ru" class="header__text header__email">623344@mail.ru</a>
+              <p class="header__text header__addres">г. Барнаул
+                <br/> ул. Южные Мастерские, д. 14 Б</p>
+            </div>
+            <div class="hr"></div>
+            <div class="top-menu">
+              <a href="#" class="top-menu__button"> <img class="top-menu__image" src="images/menu-button.svg" alt="" /><span class="top-menu__title">МЕНЮ</span> </a>
+              <ul class="top-menu__items">
+                <li class="top-menu__item"><a href="" class="top-menu__link">Продукция</a></li>
+                <li class="top-menu__item"><a href="" class="top-menu__link">Продукция</a></li>
+                <li class="top-menu__item"><a href="" class="top-menu__link">Продукция</a></li>
+                <li class="top-menu__item"><a href="" class="top-menu__link">Продукция</a></li>
+                <li class="top-menu__item"><a href="" class="top-menu__link">Продукция</a></li>
+              </ul>
+            </div>
+          </div>
+        </section>
