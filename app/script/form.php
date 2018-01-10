e6f66a3eb5ad1afacfd7817b587d51b0f647e3fd
Antoha5001 <plantsvzzombi@mail.ru>
2018-01-10 11:21:23 +0700

    edit forms

--- app/script/form.php
+++ app/script/form.php
@@ -4,14 +4,15 @@
 		$host = "Host: www.".$_SERVER["HTTP_HOST"]."<br>";
 		$remoteip = "IP-адрес: ".$_SERVER["REMOTE_ADDR"]."<br>";
 		$to  = 'photo@623030.ru' . ', '; // обратите внимание на запятую
-		//$to .= '3852@623030.ru';
+		$to .= '3852@623030.ru'. ', ';
+		$to .= '623344@mail.ru';
 		$zag = "Заявка на звонок";
 		$fname = "Имя: ".trim(strip_tags($_POST["name"]))."<br>";
 		$phone = "Телефон: ".trim(strip_tags($_POST["tel"]))."<br>";
 		$email = "email: ".trim(strip_tags($_POST["email"]))."<br>";
 		$mess = "Сообщение: ".trim(strip_tags($_POST["message"]))."<br>";
 		$headers = 'Content-type: text/html; charset=utf-8';
-		mail("$to","$zag","$host $remoteip $fname $phone $email","$headers");
+		mail("$to","$zag","$host $remoteip $fname $phone $email $mess","$headers");
 		//header("Location: ".$_SERVER['PHP_SELF']);
 		//header("Location: ../index.php");
 }
